dfd5aa172877f44fa6022b9349ec89d9a521206f
Tiago Aguiar <aguiartiago@hotmail.com>
2018-05-28 13:29:50 -0300

    create service

--- src/main/java/br/com/aguiar/merchant/controller/MerchantController.java
+++ src/main/java/br/com/aguiar/merchant/controller/MerchantController.java
@@ -4,6 +4,8 @@ import br.com.aguiar.merchant.model.Merchant;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import br.com.aguiar.merchant.service.MerchantService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.UUID;
@@ -12,19 +14,37 @@ import java.util.UUID;
 @RequestMapping("/merchants")
 public class MerchantController {
 
+    private MerchantService merchantService;
+
+    public MerchantController(MerchantService merchantService) {
+        this.merchantService = merchantService;
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Merchant> getMerchant(@PathVariable(value = "id") UUID id) {
-        Merchant body = new Merchant();
-        body.setId(id);
-        body.setName("Test");
-        return ResponseEntity.ok(body);
+    public ResponseEntity<Merchant> get(@PathVariable(value = "id") UUID id) {
+        Merchant merchantFound = merchantService.get(id);
+        return ResponseEntity.ok().body(merchantFound);
     }
 
     @PostMapping
-    public ResponseEntity<String> create(@RequestBody Merchant merchant) {
-        throw new RuntimeException("bla");
-//        URI uriLocation = URI.create("localhost:8080"+"/merchants"+"/uuid");
-//        return ResponseEntity.created(uriLocation).build();
+    public ResponseEntity<Void> create(@RequestBody Merchant merchant) {
+        Merchant merchantSaved = merchantService.save(merchant);
+        URI uriLocation = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(merchantSaved.getId()).toUri();
+        return ResponseEntity.created(uriLocation).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable(value = "id") UUID id, @RequestBody Merchant merchant) {
+        merchant.setId(id);
+        merchantService.update(merchant);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Merchant> delete(@PathVariable(value = "id") UUID id) {
+        merchantService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
---
+++ src/main/java/br/com/aguiar/merchant/service/MerchantService.java
@@ -0,0 +1,24 @@
+package br.com.aguiar.merchant.service;
+
+import br.com.aguiar.merchant.model.Merchant;
+
+import java.util.UUID;
+
+public class MerchantService {
+
+    public Merchant get(UUID id) {
+        return null;
+    }
+
+    public Merchant save(Merchant merchant) {
+        return null;
+    }
+
+    public void update(Merchant merchant) {
+
+    }
+
+    public void delete(UUID id) {
+
+    }
+}
